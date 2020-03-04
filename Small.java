9ca4bba89b8216e12a82b184e4945e94853b7810
jarrett712 <61672042+jarrett712@users.noreply.github.com>
2020-03-04 08:02:08 -0500

    Update and rename Sales.java to Small.java

--- Small.java
+++ Small.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 
-public class Sales {
+public class Small {
    // finds the smallest integer
    public static void main(String[] args) {
       Scanner input = new Scanner(System.in);
