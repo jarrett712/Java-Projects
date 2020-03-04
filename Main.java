f3e66fde69045a9fef95d92dac00d0865e2f7756
jarrett712 <61672042+jarrett712@users.noreply.github.com>
2020-03-04 07:59:58 -0500

    Add files via upload
    Finding the smallest integer from inputed values.

---
+++ Main.java
@@ -0,0 +1,29 @@
+import java.util.Scanner;
+
+public class Main {
+   // finds the smallest integer
+   public static void main(String[] args) {
+      Scanner input = new Scanner(System.in);
+      
+      int smallest = 0; // smallest number
+      int number = 0; // number entered by user
+      
+      System.out.print("Enter number of integers: ");
+      int integers = input.nextInt();
+      
+      for (int counter = 1; counter <= integers; counter++) {
+         System.out.print("Enter integer: ");
+         number = input.nextInt();
+         
+         if (counter == 1) {
+            smallest = number;
+            
+         }
+         else if (number < smallest) {
+            smallest = number;
+         }
+      }
+      
+      System.out.printf("Smallest Integer is: %d%n", smallest);
+   }
+}
